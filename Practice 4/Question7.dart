688f0e6062e20cb6b71b39e7467d34294be77ab2
Halima Akter Nila <halimaakternila48@gmail.com>
2025-10-20 21:31:01 +0600

    Update Question7.dart

--- Practice 4/Question7.dart
+++ Practice 4/Question7.dart
@@ -1,1 +1,11 @@
+void main(){
+  Map<String, String> contacts = {
+    'Hasna': '01100000000',
+    'Nila': '01200000000',
+    'Nipa': '01300000000',
+    'Halima': '01400000000'
+  };
 
+  var KL4 = contacts.keys.where((k) => k.length == 4);
+  print("Keys with length 4: $KL4");
+}
