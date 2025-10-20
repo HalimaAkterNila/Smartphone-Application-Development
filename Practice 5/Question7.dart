bb8efe4ed6fdef31a59c946f98a07321534830de
Halima Akter Nila <halimaakternila48@gmail.com>
2025-10-20 21:36:32 +0600

    Update Question7.dart

--- Practice 5/Question7.dart
+++ Practice 5/Question7.dart
@@ -1,1 +1,19 @@
+import 'dart:io';
+
+void main(){
+  File file = File('students.csv');
+
+  List<String> students = [
+    'Name,Age,Address',
+    'Halima,22,Sylhet',
+    'Nila,21,Sunamganj',
+    'Jasmin,25,Barishal'
+  ];
+
+  file.writeAsStringSync(students.join('\n'));
+  print('Students data written to students.csv\n');
+
+  String content = file.readAsStringSync();
+  print('Reading students.csv:\n$content');
+}
 
