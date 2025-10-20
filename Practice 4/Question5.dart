a21a8ac589c2e8bd62575f77a4d072de6a1a30f2
Halima Akter Nila <halimaakternila48@gmail.com>
2025-10-20 21:29:21 +0600

    Update Question5.dart

--- Practice 4/Question5.dart
+++ Practice 4/Question5.dart
@@ -1,1 +1,9 @@
-
+void main(){
+  List <String> friends = ["Anika", "Jasmin", "Halima", "Nila"];
+  print("Friends name starting with 'A' : ");
+  for (var i in friends){
+    if (i[0] == 'A'){
+      print(i);
+    }
+  }
+}
