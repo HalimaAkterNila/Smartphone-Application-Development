9aff654a1ec15db316dd094409c2087a75f0bc46
Halima Akter Nila <halimaakternila48@gmail.com>
2025-10-20 21:28:23 +0600

    Update Question3.dart

--- Practice 4/Question3.dart
+++ Practice 4/Question3.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+void main(){
+  List <int> exp = [];
+  stdout.write("Number of Expenses : ");
+  int n = int.parse(stdin.readLineSync()!);
+  int total = 0;
+
+  for (int i = 0; i < n; i++){
+    int val = int.parse(stdin.readLineSync()!);
+    exp.add(val);
+    total += val;
+  }
+  print("Total Expenses : $total");
+}
