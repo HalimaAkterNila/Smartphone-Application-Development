4e6f6cdf0afd7f51b6ae1140d42738cf7a34f329
Halima Akter Nila <halimaakternila48@gmail.com>
2025-10-20 21:34:24 +0600

    Append friend's name to hello.txt

--- Practice 5/Question2.dart
+++ Practice 5/Question2.dart
@@ -1,1 +1,7 @@
+import 'dart:io';
 
+void main(){
+  File f = File('hello.txt');
+  f.writeAsStringSync("\nMy Friend: Jasmin", mode : FileMode.append);
+  print("Friend's appended to hello.txt");
+}
