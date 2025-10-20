28b2c6eacc09d6a78df5b6785472893f48c135c9
Halima Akter Nila <halimaakternila48@gmail.com>
2025-10-20 21:30:17 +0600

    Update Question6.dart

--- Practice 4/Question6.dart
+++ Practice 4/Question6.dart
@@ -1,1 +1,14 @@
+void main(){
+  Map<String, dynamic> person = {
+    'Name': 'Halima Akter Nila',
+    'Address': 'New York',
+    'Age': 22,
+    'Country': 'America'
+  };
+  person['Country'] = 'Bangladesh';
+  
+  person.forEach((key, val){
+    print('$key: $val');
+  });
+}
 
