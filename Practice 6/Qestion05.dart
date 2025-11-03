129383c6e82e35b29ae0747c35a300f0e06554c7
Halima Akter Nila <halimaakternila48@gmail.com>
2025-11-03 20:32:48 +0600

    Update Qestion05.dart

--- Practice 6/Qestion05.dart
+++ Practice 6/Qestion05.dart
@@ -1,1 +1,30 @@
+class Camera {
+  int _id;
+  String _brand;
+  String _color;
+  double _price;
+
+  Camera(this._id, this._brand, this._color, this._price);
+
+  int get id => _id;
+  String get brand => _brand;
+  String get color => _color;
+  double get price => _price;
+
+  set price(double value) => _price = value;
+
+  void display() {
+    print("ID: $_id, Brand: $_brand, Color: $_color, Price: \$${_price}");
+  }
+}
+
+void main() {
+  Camera c1 = Camera(1, "Canon", "Black", 50000);
+  Camera c2 = Camera(2, "Nikon", "Gray", 65000);
+  Camera c3 = Camera(3, "Sony", "Silver", 80000);
+
+  c1.display();
+  c2.display();
+  c3.display();
+}
 
