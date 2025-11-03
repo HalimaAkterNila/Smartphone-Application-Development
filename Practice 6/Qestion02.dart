8a31f794972ddeee4c8a5fa05feea7a9c8a0bc81
Halima Akter Nila <halimaakternila48@gmail.com>
2025-11-03 20:31:58 +0600

    Add House class and main function to display houses

--- Practice 6/Qestion02.dart
+++ Practice 6/Qestion02.dart
@@ -1,1 +1,23 @@
+class House{
+  int id;
+  String name;
+  double price;
 
+  House(this.id, this.name, this.price);
+
+  void show(){
+    print("House ID: $id, Name: $name, Price: \$${price}");
+  }
+}
+
+void main(){
+  List<House> houses = [
+    House(1, "Green Villa", 250000),
+    House(2, "Sunset Home", 300000),
+    House(3, "Dream House", 450000)
+  ];
+
+  for (var h in houses){
+    h.show();
+  }
+}
