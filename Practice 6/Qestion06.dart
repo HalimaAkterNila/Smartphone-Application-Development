071b848c77c8f1af1cabb28d1ed1fd8fddc1fe7b
Halima Akter Nila <halimaakternila48@gmail.com>
2025-11-03 20:32:56 +0600

    Update Qestion06.dart

--- Practice 6/Qestion06.dart
+++ Practice 6/Qestion06.dart
@@ -1,1 +1,20 @@
+abstract class Bottle {
+  void open();
+}
+
+class CokeBottle implements Bottle {
+  @override
+  void open() {
+    print("Coke bottle is opened");
+  }
+
+  factory CokeBottle.create() {
+    return CokeBottle();
+  }
+}
+
+void main() {
+  Bottle coke = CokeBottle.create();
+  coke.open();
+}
 
