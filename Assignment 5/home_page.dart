4de24623c58a0e4947738990c5b0b6005cf89c10
Halima Akter Nila <halimaakternila48@gmail.com>
2025-11-24 22:23:32 +0600

    Add files via upload

---
+++ Assignment 5/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("Home Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.home, color: Colors.white)),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings, color: Colors.white),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome!",
+          style: TextStyle(fontSize: 30, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
